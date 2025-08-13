37347eec4b0a590921aad9b082a2f5ac27821a42
Broken Pipe <navigaid@gmail.com>
2025-08-14 03:31:32 +0800

    restrict index middleware to authenticated sessions

--- examples/pocket/relay.go
+++ examples/pocket/relay.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/btwiuse/proxy"
 	"github.com/pocketbase/pocketbase/core"
@@ -87,54 +88,97 @@ func RelayHookFunc(se *core.ServeEvent) error {
 	// request flow:
 	// 1. agent register as <id>
 	// 2. user visits HOST/<id>
-	// 3. update indexHandler, 302 redirected to HOST
+	// 3. set cookies, 302 redirected to HOST
 	// 4. serves agent content
-	// 5. agent disconnects, resets indexHandler
+	// 5. user visits HOST/_/, unsetting cookies
 	// 6. user visits fallback path
 
 	// pros
-	// - works for cli clients and browsers
+	// - accessible only from authenticated sessions
 
 	// cons:
-	// - accessible by everyone once exposed
+	// - browser only
+	// - expose one app at a time
+	// - browser reauthenticate needed when agent reconnects
 
-	var indexHandler http.Handler
-	var indexPath string
-
+	indexSessionKey := "_indexSessionKey"
+	unsetCookie := func(w http.ResponseWriter) {
+		http.SetCookie(w, &http.Cookie{
+			Name:     indexSessionKey,
+			Value:    "",
+			MaxAge:   -1,
+			Path:     "/",
+			Expires:  time.Unix(0, 0),
+			HttpOnly: true,
+			SameSite: http.SameSiteLaxMode,
+		})
+	}
 	indexMiddleware := &hook.Handler[*core.RequestEvent]{
 		Id: "indexMiddlewareId",
 		Func: func(re *core.RequestEvent) error {
 			w, r := re.Event.Response, re.Event.Request
 
-			isAPI := strings.HasPrefix(r.URL.Path, "/api/")
 			isUI := strings.HasPrefix(r.URL.Path, "/_/")
+			isAPI := strings.HasPrefix(r.URL.Path, "/api/")
+			isUIReferer := strings.HasSuffix(r.Referer(), "/_/")
 
-			if s.IsRootExternal(r) && !isAPI && !isUI {
+			if s.IsRootExternal(r) && isAPI && isUIReferer {
+				// passthrough requests to /api/... from /_/
+				goto NEXT
+			}
+
+			if s.IsRootExternal(r) && isUI {
+				// reset session cookie on visiting /_/
+				unsetCookie(w)
+				goto NEXT
+			}
+
+			if s.IsRootExternal(r) && !isUI {
 				// get path => id
 				rpath := leadingComponent(r.URL.Path)
+
+				// set session cookie
 				if rt, ok := s.GetRoundTripper(rpath); ok {
-					// set subsequent calls
+					// enable reverse proxy
 					rp := utils.LoggedReverseProxy(rt)
 					rp.Rewrite = func(req *httputil.ProxyRequest) {
 						req.SetXForwarded()
 						req.Out.URL.Host = r.Host
 						req.Out.URL.Scheme = "http"
 					}
-					indexHandler = rp
-					indexPath = rpath
-					// redirect to /
+					re.App.Store().Set(rpath, rp)
+					// set cookies and redirect to /
+					http.SetCookie(w, &http.Cookie{
+						Name:     indexSessionKey,
+						Value:    rpath,
+						MaxAge:   2592000, // 30 days in seconds
+						Path:     "/",
+						HttpOnly: true,
+						SameSite: http.SameSiteLaxMode,
+					})
 					http.Redirect(w, r, "/", http.StatusFound)
 					return nil
-				} else if indexHandler != nil {
-					if _, ok := relay.DefaultStorage.LookupRecord(indexPath); ok {
-						indexHandler.ServeHTTP(w, r)
-						return nil
+				} else {
+					// check cookie
+					cookie, err := r.Cookie(indexSessionKey)
+					if err != nil {
+						goto NEXT
 					}
-					indexPath = ""
-					indexHandler = nil
+					if _, ok := relay.DefaultStorage.LookupRecord(cookie.Value); ok {
+						// serve subsequent calls
+						rp, ok := re.App.Store().GetOk(cookie.Value)
+						if ok {
+							rp.(http.Handler).ServeHTTP(w, r)
+							return nil
+						}
+					} else {
+						// invalidate cookie
+						unsetCookie(w)
+					}
 				}
 			}
 
+		NEXT:
 			return re.Next()
 		},
 		Priority: 3,
