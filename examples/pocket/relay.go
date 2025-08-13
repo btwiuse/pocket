5a7a5928ebc17d029345838a9d76da051d20f68d
Broken Pipe <navigaid@gmail.com>
2025-08-14 01:17:30 +0800

    add experimental index middleware

--- examples/pocket/relay.go
+++ examples/pocket/relay.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"net/http/httputil"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/btwiuse/proxy"
 	"github.com/pocketbase/pocketbase/core"
@@ -30,6 +33,10 @@ func StringToInt(str string, fallback int) int {
 		return fallback
 	}
 	return num
+}
+
+func leadingComponent(s string) string {
+	return strings.Split(strings.TrimPrefix(s, "/"), "/")[0]
 }
 
 func RelayHookFunc(se *core.ServeEvent) error {
@@ -77,6 +84,62 @@ func RelayHookFunc(se *core.ServeEvent) error {
 		Priority: 2,
 	}
 
+	// request flow:
+	// 1. agent register as <id>
+	// 2. user visits HOST/<id>
+	// 3. update indexHandler, 302 redirected to HOST
+	// 4. serves agent content
+	// 5. agent disconnects, resets indexHandler
+	// 6. user visits fallback path
+
+	// pros
+	// - works for cli clients and browsers
+
+	// cons:
+	// - accessible by everyone once exposed
+
+	var indexHandler http.Handler
+	var indexPath string
+
+	indexMiddleware := &hook.Handler[*core.RequestEvent]{
+		Id: "indexMiddlewareId",
+		Func: func(re *core.RequestEvent) error {
+			w, r := re.Event.Response, re.Event.Request
+
+			isAPI := strings.HasPrefix(r.URL.Path, "/api/")
+			isUI := strings.HasPrefix(r.URL.Path, "/_/")
+
+			if s.IsRootExternal(r) && !isAPI && !isUI {
+				// get path => id
+				rpath := leadingComponent(r.URL.Path)
+				if rt, ok := s.GetRoundTripper(rpath); ok {
+					// set subsequent calls
+					rp := utils.LoggedReverseProxy(rt)
+					rp.Rewrite = func(req *httputil.ProxyRequest) {
+						req.SetXForwarded()
+						req.Out.URL.Host = r.Host
+						req.Out.URL.Scheme = "http"
+					}
+					indexHandler = rp
+					indexPath = rpath
+					// redirect to /
+					http.Redirect(w, r, "/", http.StatusFound)
+					return nil
+				} else if indexHandler != nil {
+					if _, ok := relay.DefaultStorage.LookupRecord(indexPath); ok {
+						indexHandler.ServeHTTP(w, r)
+						return nil
+					}
+					indexPath = ""
+					indexHandler = nil
+				}
+			}
+
+			return re.Next()
+		},
+		Priority: 3,
+	}
+
 	ingressMiddleware := &hook.Handler[*core.RequestEvent]{
 		Id: "ingressMiddlewareId",
 		Func: func(re *core.RequestEvent) error {
@@ -89,12 +152,14 @@ func RelayHookFunc(se *core.ServeEvent) error {
 
 			return re.Next()
 		},
-		Priority: 3,
+		Priority: 4,
 	}
 
 	se.Router.Bind(proxyMiddleware)
 
 	se.Router.Bind(upgradeMiddleware)
+
+	se.Router.Bind(indexMiddleware)
 
 	se.Router.Bind(ingressMiddleware)
 
